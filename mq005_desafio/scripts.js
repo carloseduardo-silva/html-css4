8a40b34eb03be06112df037d09572f812f6a83ff
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-08-24 12:30:05 -0300

    projeto menu mq005

---
+++ mq005_desafio/scripts.js
@@ -0,0 +1,7 @@
+
+function clicar(){
+    resp = document.getElementById('respostareal')
+    resp.innerHTML = '<ul> <li> 01 </li>   <li> 02 </li>   <li> 03 </li>  <li> 04 </li>  <li> 05 </li> </ul>'
+         
+}
+    
