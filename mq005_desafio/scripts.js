b7306845445ac19cdf285a09a584fb8493df6ca5
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-08-31 14:03:36 -0300

    projeto css html js finalizado

--- mq005_desafio/scripts.js
+++ mq005_desafio/scripts.js
@@ -1,17 +1,37 @@
 
+
+
+
 function clicar(){
      resp = document.getElementById('res')
      var margem = document.getElementById('cabeca')
+  
      if (resp.style.display == 'block') {
          resp = resp.style.display = 'none'
          margem = margem.style.marginBottom = '2vh'
      }
+     
+    
+
      else {
         resp.style.display = 'block'
         margem = margem.style.marginBottom = '25vh'
      }
 
          
+}
+
+function mudouTamanho(){
+    resp = document.getElementById('res')
+    var margem = document.getElementById('cabeca')
+    if (window.innerWidth >= 750){
+        resp.style.display = 'block';
+        margem.style.marginBottom = '2vh';
+
+    }
+    else {
+        resp.style.display = 'none'
+    }
 }
     
 
